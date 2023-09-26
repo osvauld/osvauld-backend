b57dc01785047e115fc82474d76e3a0d6aeef608
Anoop Jacob Kottoor <anoopjacobkottoor@Anoops-MacBook-Air.local>
2023-09-27 00:59:01 +0530

    Change user response type in group users API

--- src/main/kotlin/com/shadowsafe/secretsmanagerbackend/usermanagement/usergroups/dto/GetUsersInGroupsResponseDTO.kt
+++ src/main/kotlin/com/shadowsafe/secretsmanagerbackend/usermanagement/usergroups/dto/GetUsersInGroupsResponseDTO.kt
@@ -1,8 +1,9 @@
 package com.shadowsafe.secretsmanagerbackend.usermanagement.usergroups.dto
 
 import com.shadowsafe.secretsmanagerbackend.shared.IDTO
+import com.shadowsafe.secretsmanagerbackend.usermanagement.users.dto.UsersResponseDTO
 import com.shadowsafe.secretsmanagerbackend.usermanagement.users.model.UsersEntity
 
 data class GetUsersInGroupsResponseDTO(
-    val users: List<UsersEntity>,
+    val users: List<UsersResponseDTO>,
 ) : IDTO
